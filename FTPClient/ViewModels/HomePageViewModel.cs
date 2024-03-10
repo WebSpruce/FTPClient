db270a924434f69e49370f30bfe76d474746d971
Kacper Świerczek <kacperswi12363@gmail.com>
2024-03-10 19:42:12 +0100

    feat: homepage input regex, bindings

--- FTPClient/ViewModels/HomePageViewModel.cs
+++ FTPClient/ViewModels/HomePageViewModel.cs
@@ -1,6 +1,60 @@
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+using Avalonia.Controls;
+using Avalonia.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+
 namespace FTPClient.ViewModels;
 
-public class HomePageViewModel : ViewModelBase
+public partial class HomePageViewModel : ViewModelBase
 {
     
+    private string _host;
+    public string Host
+    {
+        get => _host;
+        set
+        {
+            if (Regex.IsMatch(value, @"^[\d\.]*$"))
+            {
+                SetProperty(ref _host, value);
+            }
+        }
+    }
+    [ObservableProperty] 
+    private string _username;
+    [ObservableProperty] 
+    private string _password;
+    private string _port;
+    public string Port
+    {
+        get => _port;
+        set
+        {
+            if (Regex.IsMatch(value, @"^\d*$"))
+            {
+                SetProperty(ref _port, value);
+            }
+        }
+    }
+    [ObservableProperty] 
+    private bool _connectBtnVisibility = true;
+    [ObservableProperty] 
+    private bool _disconnectBtnVisibility;
+    
+    [RelayCommand]
+    private void ConnectToServer()
+    {
+        Debug.WriteLine($"info: {Host} - {Username} - {Password} - {Port}");
+        ConnectBtnVisibility = !ConnectBtnVisibility;
+        DisconnectBtnVisibility = !DisconnectBtnVisibility;
+    }
+    [RelayCommand]
+    private void DisconnectFromServer()
+    {
+        Debug.WriteLine($"Disconnect");
+        ConnectBtnVisibility = !ConnectBtnVisibility;
+        DisconnectBtnVisibility = !DisconnectBtnVisibility;
+    }
 }
